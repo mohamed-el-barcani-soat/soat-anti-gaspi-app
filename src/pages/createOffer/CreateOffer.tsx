52152e6a517623a9790bd59554cf257bc603fc09
MasatakaIshii <masaradio78@gmail.com>
2022-10-19 00:48:50 +0200

    refacto validation schema and show created offer id

--- src/pages/createOffer/CreateOffer.tsx
+++ src/pages/createOffer/CreateOffer.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
 import {
   Button,
   Card,
@@ -14,55 +13,7 @@ import "./CreateOffer.css";
 import { CreateOfferInputs } from "./CreateOfferInputs";
 import OffersService from "../../services/OffersService";
 import { CreateOfferRequest } from "../../services/models/offer";
-
-const userSchema = yup.object({
-  username: yup
-    .string()
-    .required("Le username est requis")
-    .max(250, "Le username doit être inférieur à 250 charactères"),
-  email: yup
-    .string()
-    .required("Email is required")
-    .email("Email must be valid"),
-});
-
-const addressSchema = yup.object({
-  streetNumber: yup
-    .number()
-    .typeError("Le numéro de la rue doit être un nombre")
-    .required("Le numéro de la rue est requis")
-    .min(1, "Le numéro de la rue doit être supérieur ou égale à 1"),
-  country: yup
-    .string()
-    .required("Le pays est requis")
-    .max(250, "Le pays doit être inférieur à 250 charactères"),
-  street: yup.string().required("La rue est requise"),
-  city: yup
-    .string()
-    .required("La ville est requise")
-    .max(250, "La ville doit être inférieur à 250 charactères"),
-  zipcode: yup
-    .string()
-    .required("Le code postal est requis")
-    .max(250, "Le code postal doit être inférieur à 250 charactères"),
-});
-
-const createOfferSchema = yup.object({
-  user: userSchema,
-  title: yup
-    .string()
-    .required("Le titre est requis")
-    .max(250, "Le titre doit être inférieur à 250 charactères"),
-  address: addressSchema,
-  availability: yup
-    .date()
-    .typeError("La date de disponibilité est requise")
-    .required("La date de disponibilité est requise"),
-  expiration: yup
-    .date()
-    .typeError("La date d'expiration est requise")
-    .required("La date d'expiration est requise"),
-});
+import createOfferSchema from "./createOfferSchema";
 
 const CreateOffer: React.FC = () => {
   const {
@@ -73,6 +24,7 @@ const CreateOffer: React.FC = () => {
     resolver: yupResolver(createOfferSchema),
   });
   const [isLoading, setIsLoading] = useState(false);
+  const [offerId, setOfferId] = useState<string>();
 
   const submitCreateOffer = handleSubmit(async (data) => {
     if (data.address.streetNumber) {
@@ -84,7 +36,7 @@ const CreateOffer: React.FC = () => {
         },
       };
       const id = await OffersService.createOffer(request);
-      console.log(id);
+      setOfferId(id);
     }
   });
 
@@ -160,8 +112,8 @@ const CreateOffer: React.FC = () => {
           </div>
           {isLoading && <Loading />}
           {!isLoading && <Button type="submit">Créer</Button>}
-          {/* {hasError && <Error refresh={() => submitCreateOffer()} />}
-          {offerId && <p>L'annonce {offerId} a bien été créée !</p>} */}
+          {/* {hasError && <Error refresh={() => submitCreateOffer()} />} */}
+          {offerId && <p>L'annonce {offerId} a bien été créée !</p>}
         </CardBody>
       </Card>
     </form>
