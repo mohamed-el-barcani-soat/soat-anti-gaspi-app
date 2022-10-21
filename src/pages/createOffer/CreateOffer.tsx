fef42cc866279e0a3dd9268978ce110568cd0be5
MasatakaIshii <masaradio78@gmail.com>
2022-10-21 08:01:27 +0200

    improvement(create-offer): create offer with redirection to list offer after creation

--- src/pages/createOffer/CreateOffer.tsx
+++ src/pages/createOffer/CreateOffer.tsx
@@ -14,6 +14,7 @@ import { CreateOfferInputs } from "./CreateOfferInputs";
 import OffersService from "../../services/OffersService";
 import { CreateOfferRequest } from "../../services/models/offer";
 import createOfferSchema from "./createOfferSchema";
+import { Link } from "react-router-dom";
 
 const CreateOffer: React.FC = () => {
   const {
@@ -24,7 +25,7 @@ const CreateOffer: React.FC = () => {
     resolver: yupResolver(createOfferSchema),
   });
   const [isLoading, setIsLoading] = useState(false);
-  const [offerId, setOfferId] = useState<string>();
+  const [isCreated, setIsCreated] = useState(false);
 
   const submitCreateOffer = handleSubmit(async (data) => {
     if (data.address.streetNumber) {
@@ -36,8 +37,14 @@ const CreateOffer: React.FC = () => {
         availabilityDate: data.availability,
         expirationDate: data.expiration,
       };
-      const id = await OffersService.createOffer(request);
-      setOfferId(id);
+      try {
+        setIsLoading(true);
+        await OffersService.createOffer(request);
+        setIsCreated(true);
+        setIsLoading(false);
+      } catch (e) {
+        console.error(e);
+      }
     }
   });
 
@@ -46,75 +53,85 @@ const CreateOffer: React.FC = () => {
       <Card>
         <CardHeader>Créer une annonce</CardHeader>
         <CardBody>
-          <div className="form-content">
-            <Input
-              label="Titre :"
-              {...register("title")}
-              error={errors.title?.message}
-            />
-            <Input
-              label="Description :"
-              {...register("description")}
-              error={errors.description?.message}
-            />
-            <Input
-              label="Username :"
-              {...register("user.username")}
-              error={errors.user?.username?.message}
-            />
-            <Input
-              type="email"
-              label="Email :"
-              {...register("user.email")}
-              error={errors.user?.email?.message}
-            />
-            <Input
-              label="Ville :"
-              {...register("address.city")}
-              error={errors.address?.city?.message}
-            />
-            <Input
-              label="Pays :"
-              {...register("address.country")}
-              error={errors.address?.country?.message}
-            />
-            <Input
-              label="Rue :"
-              {...register("address.street")}
-              error={errors.address?.street?.message}
-            />
-            <Input
-              label="Numéro de rue :"
-              {...register("address.streetNumber")}
-              error={errors.address?.streetNumber?.message}
-            />
-            <Input
-              label="Indicateur :"
-              {...register("address.streetNumberIndicator")}
-              error={errors.address?.streetNumberIndicator?.message}
-            />
-            <Input
-              label="Code postal :"
-              {...register("address.zipcode")}
-              error={errors.address?.zipcode?.message}
-            />
-            <Input
-              type="date"
-              label="Disponibilité :"
-              {...register("availability")}
-              error={errors.availability?.message}
-            />
-            <Input
-              type="date"
-              label="Expiration :"
-              {...register("expiration")}
-              error={errors.expiration?.message}
-            />
-          </div>
+          {!isCreated && (
+            <div className="form-content">
+              <Input
+                label="Titre :"
+                {...register("title")}
+                error={errors.title?.message}
+              />
+              <Input
+                label="Description :"
+                {...register("description")}
+                error={errors.description?.message}
+              />
+              <Input
+                label="Username :"
+                {...register("user.username")}
+                error={errors.user?.username?.message}
+              />
+              <Input
+                type="email"
+                label="Email :"
+                {...register("user.email")}
+                error={errors.user?.email?.message}
+              />
+              <Input
+                label="Ville :"
+                {...register("address.city")}
+                error={errors.address?.city?.message}
+              />
+              <Input
+                label="Pays :"
+                {...register("address.country")}
+                error={errors.address?.country?.message}
+              />
+              <Input
+                label="Rue :"
+                {...register("address.street")}
+                error={errors.address?.street?.message}
+              />
+              <Input
+                label="Numéro de rue :"
+                {...register("address.streetNumber")}
+                error={errors.address?.streetNumber?.message}
+              />
+              <Input
+                label="Indicateur :"
+                {...register("address.streetNumberIndicator")}
+                error={errors.address?.streetNumberIndicator?.message}
+              />
+              <Input
+                label="Code postal :"
+                {...register("address.zipcode")}
+                error={errors.address?.zipcode?.message}
+              />
+              <Input
+                type="date"
+                label="Disponibilité :"
+                {...register("availability")}
+                error={errors.availability?.message}
+              />
+              <Input
+                type="date"
+                label="Expiration :"
+                {...register("expiration")}
+                error={errors.expiration?.message}
+              />
+            </div>
+          )}
           {isLoading && <Loading />}
-          {!isLoading && <Button type="submit">Créer</Button>}
+          {!isCreated && !isLoading && <Button type="submit">Créer</Button>}
           {/* {hasError && <Error refresh={() => submitCreateOffer()} />} */}
-          {offerId && <p>L'annonce {offerId} a bien été créée !</p>}
+          {isCreated && (
+            <>
+              <p>
+                L'annonce a bien été créée ! Vous allez recevoir un mail pour
+                valider votre annonce.
+              </p>
+              <Link to="/offers">Revenir à la liste des annonces</Link>
+            </>
+          )}
         </CardBody>
       </Card>
     </form>
